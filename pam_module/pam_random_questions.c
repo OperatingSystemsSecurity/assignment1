972038078a423a18fec8afb6af4a01267fb4127a
Hendrikto <hendrik.to@gmail.com>
2017-11-21 18:20:25 +0100

    Add PAM module template.

---
+++ pam_module/pam_random_questions.c
@@ -0,0 +1,15 @@
+// author: Hendrik Werner s4549775
+
+#define PAM_SM_AUTH
+
+#include <security/pam_appl.h>
+#include <security/pam_modules.h>
+
+PAM_EXTERN int pam_sm_authenticate(
+	pam_handle_t *pamh
+	, int flags
+	, int argc
+	, const char **argv
+) {
+	return PAM_SUCCESS;
+}
